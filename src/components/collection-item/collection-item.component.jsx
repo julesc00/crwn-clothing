f7110ca937bbd51ed26e323dd85955fff81668bf
Julio Briones <juliobriones@pop-os.localdomain>
2019-11-15 05:27:38 -0600

    trivial style change

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./collection-item.styles.scss";
 
-
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
     <div
@@ -9,12 +8,11 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
-    >
+    />
       <div className="collection-footer">
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-    </div>
   </div>
 );
 
